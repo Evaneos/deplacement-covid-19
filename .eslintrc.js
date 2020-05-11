eec0732efe2016a294236ad52d3f38d5f2536751
Antoine Lépée <alepee@gmail.com>
2020-05-11 11:27:55 +0200

    fix build pipeline

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
 	root: true,
 	env: {
-        node: true,
-        browser: true,
+    node: true,
+    browser: true,
 	},
 	extends: ["standard"],
 	rules: {
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"comma-dangle": [2, "always-multiline"],
+		"no-var": 2,
 	},
 	overrides: [
 		{
