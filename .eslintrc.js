6be0d22147206c918c5fffe6660650755354b97f
Antoine Lépée <alepee@gmail.com>
2020-05-11 11:20:04 +0200

    remove eslint specific rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
 	root: true,
 	env: {
-    node: true,
-    browser: true,
+        node: true,
+        browser: true,
 	},
 	extends: ["standard"],
 	rules: {
-		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-		"comma-dangle": [2, "always-multiline"],
-		"no-var": 2,
 	},
 	overrides: [
 		{
